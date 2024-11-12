b2d7f45f463ccfc1c502982a5b968344ce7658ce
Adam Bidi <adam.bidi@etu.minesparis.psl.eu>
2024-11-12 15:03:14 +0100

    commit du contact du snake

--- snake2.py
+++ snake2.py
@@ -28,6 +28,37 @@ snake = [(4,5),(5,5),(6,5)]
 direction = (1,0)
 fruit_x, fruit_y = randint(0,20), randint(0,20)
 
+def handle_event(event):
+    # chaque évênement à un type qui décrit la nature de l'évênement
+    # un type de pg.QUIT signifie que l'on a cliqué sur la "croix" de la fenêtre
+    if event.type == pg.QUIT:
+        running = False
+    # un type de pg.KEYDOWN signifie que l'on a appuyé une touche du clavier
+    elif event.type == pg.KEYDOWN:
+        # si la touche est "Q" on veut quitter le programme
+        if event.key == pg.K_q:
+            running = False
+        if event.key == pg.K_UP:
+            if (direction_x, direction_y)== (0,1):
+                pass
+            else :
+                direction = (0,-1)
+        if event.key == pg.K_DOWN:
+            if direction == (0,-1):
+                pass
+            else :
+                direction = (0,1)
+        if event.key == pg.K_LEFT:
+            if direction == (1,0):
+                pass
+            else :
+                direction = (-1,0)
+        if event.key == pg.K_RIGHT:
+            if direction == (-1,0):
+                pass
+            else :
+                direction = (1,0)
+
 while running:
     clock.tick(10)
     screen.fill(white)
@@ -41,7 +72,10 @@ while running:
     first_x, first_y = snake[0]
     direction_x, direction_y = direction
     snake.pop()
-    snake.insert(0, (first_x + direction_x, first_y + direction_y))
+    if (first_x + direction_x, first_y + direction_y) in snake:
+        running = False
+    else :
+        snake.insert(0, (first_x + direction_x, first_y + direction_y))
     
    
     fruit = (fruit_x, fruit_y)
@@ -61,23 +95,7 @@ while running:
     # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
     # ici donc tous les évènements survenus durant la seconde précédente
     for event in pg.event.get():
-        # chaque évênement à un type qui décrit la nature de l'évênement
-        # un type de pg.QUIT signifie que l'on a cliqué sur la "croix" de la fenêtre
-        if event.type == pg.QUIT:
-            running = False
-        # un type de pg.KEYDOWN signifie que l'on a appuyé une touche du clavier
-        elif event.type == pg.KEYDOWN:
-            # si la touche est "Q" on veut quitter le programme
-            if event.key == pg.K_q:
-                running = False
-            if event.key == pg.K_UP:
-                direction = (0,-1)
-            if event.key == pg.K_DOWN:
-                direction = (0,1)
-            if event.key == pg.K_LEFT:
-                direction = (-1,0)
-            if event.key == pg.K_RIGHT:
-                direction = (1,0)
+        handle_event(event)
             
 
 
